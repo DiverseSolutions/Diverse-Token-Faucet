d284a69058727e59bb064f0d428ce57f055d510b
mnkhod <mnkhod.dev@gmail.com>
2022-04-15 22:10:58 +0800

    Added Description To Open In Metamask Browser

--- src/components/NoMetamask.js
+++ src/components/NoMetamask.js
@@ -3,6 +3,7 @@ export default function NoMetamask(){
     <>
       <div className="flex items-center justify-center h-screen">
         <h1 className="text-3xl font-semibold">No Metamask Detected</h1>
+        <h1 className="mt-1 text-lg">Open In Metamask Browser If On Mobile</h1>
       </div>
     </>
   )
