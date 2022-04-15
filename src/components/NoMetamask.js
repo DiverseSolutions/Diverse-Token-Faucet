afa5e48045454558b76528b0a1e73b8a76a3b077
mnkhod <mnkhod.dev@gmail.com>
2022-04-15 22:22:38 +0800

    Fixing Flex-Row to Flex-Col

--- src/components/NoMetamask.js
+++ src/components/NoMetamask.js
@@ -1,7 +1,7 @@
 export default function NoMetamask(){
   return (
     <>
-      <div className="flex items-center justify-center h-screen">
+      <div className="flex flex-col items-center justify-center h-screen">
         <h1 className="text-3xl font-semibold">No Metamask Detected</h1>
         <h1 className="mt-1 text-lg">Open In Metamask Browser If On Mobile</h1>
       </div>
